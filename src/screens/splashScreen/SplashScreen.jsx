19ca6efdc533c465dc85d54a7717d53a6fa9af32
misha15w <mykhailo.iv@gowombat.team>
2021-07-21 17:40:43 +0300

    splashScreen

--- src/screens/splashScreen/SplashScreen.jsx
+++ src/screens/splashScreen/SplashScreen.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { SafeAreaView, View, Text } from 'react-native'
 import { Link } from 'react-router-native'
+import { styles } from './styles'
 
 const SplashScreen = () => {
-    return (
-        <View>
-            <Text>SplashScreen</Text>
-            <Link to="/login">
-                <Text>Login</Text>
-            </Link>
-            <Link to="/signup">
-                <Text>SignUp</Text>
-            </Link>
-        </View>
-    )
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.topContainer}>
+				<Text>SplashScreen</Text>
+			</View>
+			<View style={styles.bottomContainer}>
+				<Link to='/login'>
+					<Text>Login</Text>
+				</Link>
+				<Link to='/signup'>
+					<Text>SignUp</Text>
+				</Link>
+			</View>
+		</SafeAreaView>
+	)
 }
 
-export default SplashScreen;
+export default SplashScreen
---
+++ src/screens/splashScreen/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+	container: {
+		height: '100%',
+		backgroundColor: '#999',
+		justifyContent: 'space-between',
+	},
+})
