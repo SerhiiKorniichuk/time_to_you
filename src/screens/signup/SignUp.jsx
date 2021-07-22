9935ac15dbca988301b469c6f01b77e95a550d2e
Serhii <skorniychyk@gmail.com>
2021-07-22 11:41:38 +0300

    added: fonts

--- src/screens/signup/SignUp.jsx
+++ src/screens/signup/SignUp.jsx
@@ -5,13 +5,16 @@ import { styles } from './styles'
 
 const SignUp = () => {
 	return (
-		<View style={ styles.container }>
+		<View style={styles.container}>
 			<ImageBackground
-				style={ styles.background }
-				source={require('./../../assets/images/SingUp/sing_up_bg.jpg')}
-				resizeMode='cover'
+				style={styles.background}
+				source={require('../../assets/images/SingUp/sing_up_bg.jpg')}
+				resizeMode={'cover'}
 			>
-				<Text>SingUp!</Text>
+				<View style={styles.block}>
+					<Text style={styles.title}>Welcome to&nbsp;TimetoU!</Text>
+					<Text style={styles.subtitle}>How you will use our application?</Text>
+				</View>
 			</ImageBackground>
 		</View>
 	)
--- src/screens/signup/styles.js
+++ src/screens/signup/styles.js
@@ -6,6 +6,28 @@ export const styles = StyleSheet.create({
 		flex: 1
 	},
 	background: {
-		flex: 1
+		flex: 1,
+		justifyContent: 'flex-end'
+	},
+	block: {
+		padding: 32,
+		height: '83%',
+		alignItems: 'center',
+		borderRadius: 20,
+		backgroundColor: '#FFFFFF',
+		overflow: 'hidden'
+	},
+	title: {
+		paddingTop: 58,
+		width: 181,
+		fontSize: 32,
+		fontFamily: 'Prata',
+		textAlign: 'center'
+	},
+	subtitle: {
+		paddingTop: 14,
+		fontSize: 16,
+		fontFamily: 'Poppins',
+		color: '#8E8E8E'
 	}
 })
