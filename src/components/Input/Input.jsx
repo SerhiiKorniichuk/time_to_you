fca779a9ddde517ae379b1047b1f8d86cf9fa191
Serhii <skorniychyk@gmail.com>
2021-07-23 17:10:46 +0300

    added PropTypes for Input component & refact styles

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import { TextInput, View, Text } from 'react-native'
 import { styles } from './stylesInput'
 
-export const Input = ({ label, type, onChange, value, placeholder }) => {
+
+export const Input = ({label, type, onChange, value, placeholder }) => {
 	return (
 		<View style={styles.container}>
 			{label && <Text style={styles.label}>{label}</Text>}
@@ -15,4 +17,13 @@ export const Input = ({ label, type, onChange, value, placeholder }) => {
 			/>
 		</View>
 	)
+}
+
+
+Input.propTypes = {
+	label: PropTypes.string,
+	type: PropTypes.oneOf(['password']),
+	onChange: PropTypes.func,
+	value: PropTypes.string,
+	placeholder: PropTypes.string
 }
