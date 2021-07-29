ca69c44873b2c132b0e5c664aa8fd75514647e38
misha15w <mykhailo.iv@gowombat.team>
2021-07-29 12:28:48 +0300

    add onboarding

--- src/routes/PrivateRoutes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes/PrivateRoutes.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Route } from 'react-router-native'
+import Onboarding from '../../screens/Onboarding/Onboarding'
 
 const PrivateRoutes = () => {
 	return (
-		<></>
+		<>
+			<Route exact path={'/onboarding'} component={Onboarding} />
+		</>
 	)
 }
 
